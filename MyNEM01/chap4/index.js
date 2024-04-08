e724708d796197bd84f4a346ceb15f7d4cdd7c32
Marco <marcojb.desa@gmail.com>
2024-04-08 12:39:22 +0200

    views

--- MyNEM01/chap4/index.js
+++ MyNEM01/chap4/index.js
@@ -3,7 +3,7 @@ const express = require('express') // require express module
 const app = new express() // calls express function to start new Express app
 const ejs = require('ejs')
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
@@ -12,15 +12,19 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+    //res.sendFile(path.resolve(__dirname, 'index.html'))
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about');
 })
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact');
 })
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
