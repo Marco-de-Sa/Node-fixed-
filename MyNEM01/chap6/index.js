1013b78782e86f6c459baa8ea07fe004261adbb1
Marco <marcojb.desa@gmail.com>
2024-04-08 18:11:54 +0200

    version corrections

--- MyNEM01/chap6/index.js
+++ MyNEM01/chap6/index.js
@@ -1,32 +1,35 @@
-const path = require('path')
 const express = require('express') // require express module
+const path = require('path')
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://localhost/my_database',{ useNewUrlParser: true })
+
 const app = new express() // calls express function to start new Express app
 const ejs = require('ejs')
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:true})
-
-app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
 })
 
 app.get('/', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'index.html'))
     res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
     res.render('about');
 })
 app.get('/contact', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact');
 })
 app.get('/post', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
     res.render('post')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
 })
