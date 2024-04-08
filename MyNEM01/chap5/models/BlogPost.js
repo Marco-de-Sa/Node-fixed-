9941132b7b72d730780b71d36381491f472b02d9
Marco <marcojb.desa@gmail.com>
2024-04-08 17:38:01 +0200

    blogpost

--- MyNEM01/chap5/models/BlogPost.js
+++ MyNEM01/chap5/models/BlogPost.js
@@ -5,5 +5,11 @@ const BlogPostSchema = new Schema({
     body: String
 });
 
-const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 module.exports = BlogPost
+
+BlogPost.find({
+    title: 'The Mythbuster’s Guide to Saving Money on Energy Bills'
+}, (error, blogspot) => {
+    console.log(error, blogspot)
+})
