a3dcd0e0a792b10be019a8937e3774870c15e20d
Marco <marcojb.desa@gmail.com>
2024-04-12 12:55:58 +0200

    chap10 finished

--- MyNEM01/chap10/index.js
+++ MyNEM01/chap10/index.js
@@ -16,6 +16,8 @@ const getPostController = require('./controllers/getPost');
 const storePostController = require('./controllers/storePost');
 const newUserController = require('./controllers/newUser')
 const storeUserController = require('./controllers/storeUser')
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
 //
 
 //
@@ -62,6 +64,10 @@ app.post('/posts/store', storePostController);
 app.get('/auth/register', newUserController)
 
 app.post('/users/register', storeUserController)
+
+app.get('/auth/login', loginController);
+
+app.post('/users/login',loginUserController)
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
